95a8022f57b46c85baf42595e6cf15f73d55fa7e
Salambel <boom00@ukr.net>
2019-01-12 00:10:58 +0300

    Start commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+import styled from 'styled-components'
+class App extends Component {
+  render() {
+    return (
+ <div className="wrapper">
+<SayFullName name="Ivan" surname="Pupkin" link= "vk.com" />
+<SayFullName name="anna" surname="Pupk" link= "tele.com" />
+<SayFullName name="Iv" surname="Pukin" link= "#" />
+</div>
+    );
+  }
+}
+
+function SayFullName(props){
+  return (
+<div>
+<h1>Moе имя {props.name}, фамилия -{props.surname} </h1>
+<a href={props.link}> Сылка на профиль </a>
+</div>
+    )
+}
+
+export default App;
